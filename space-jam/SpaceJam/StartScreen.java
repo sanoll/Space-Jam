8396e45dcf16089a91b3a2e80cacacf2f91e527e
sanoll <42753773+sanoll@users.noreply.github.com>
2019-04-04 19:39:39 -0500

    start screen yeet

---
+++ space-jam/SpaceJam/StartScreen.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The start up screen for the Space Jam Game.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartScreen extends ScrollingWorld
+{
+    GreenfootSound myMusic = new GreenfootSound("Space_Jam_Theme_Song.mp3"); 
+    /**
+     * Constructor for objects of class StartScreen.
+     * 
+     */
+    public StartScreen()
+    { 
+
+        // Create a new world with 600x600 cells with a cell size of 1x1 pixels.
+        super(600, 600, 1, true);
+
+
+
+    }
+
+
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("enter"))
+        Greenfoot.setWorld(new SpaceWorld());
+        myMusic.play();
+        myMusic.setVolume(30);
+    }
+}
